83edbb9a24aa668641bbcf99de064cbe7b5117f4
SeungilHan <tmddlf8484@mail.com>
2020-12-16 02:34:07 +0900

    add_terminal_command

--- src/tram_server.py
+++ src/tram_server.py
@@ -11,7 +11,8 @@ start_flag =False
 e_stop_flag =False
 app = Flask(__name__)
 ask = Ask(app, "/")
-
+NEWCODE="rostopic pub /test_flag std_msgs/Bool false --once"
+#waypoint="roslaunch waypoint_maker waypoint_loader.launch multi_lane_csv:=/home/usrg/waypoint/ki_to_presi_si4.csv"
 # ROS node, publisher, and parameter.
 # The node is started in a separate thread to avoid conflicts with Flask.
 # The parameter *disable_signals* must be set if node is not initialized
@@ -22,23 +23,22 @@ start_pub = rospy.Publisher('start_pub', Bool, queue_size=1)
 e_stop_pub = rospy.Publisher('e_stop_pub', Bool, queue_size=1)
 NGROK = rospy.get_param('/ngrok', None)
 
+def test_cb(msg):
+
+    print("hi")
 
 @ask.launch
 def launch():
     welcome_sentence = 'Welcome to U s r g Tram. where do you want to go?'
     return question(welcome_sentence)
-
-
+   
 @ask.intent('TestIntent', default={'goal': 'Main building'})
 def test_intent_function(goal):
-    '''
-    Executed when "TestIntent" is called:
-    say "Alexa, ask tester to say (first name of a person)"
-    Note that the 'intent_name' argument of the decorator @ask.intent
-    must match the name of the intent in the Alexa skill.
-    '''
+    
+
     start_flag=True
     start_pub.publish(start_flag)
+    #os.system(waypoint) 
     return statement('Hello Everyone. Thank you for riding!. \
     U s r g Tram is leaving soon. Fasten your seat belt please!.\
     It will take 6minutes to go to : {0}.'.format(goal))
@@ -46,7 +46,6 @@ def test_intent_function(goal):
 @ask.intent('EmergencyIntent')
 def Emergency_intent_function():
 
-    print("possible?")
     e_stop_flag=True
     e_stop_pub.publish(e_stop_flag)
     return statement('Emergecy stop')
@@ -61,7 +60,7 @@ def session_ended():
 if __name__ == '__main__':
     if NGROK:
         print 'NGROK mode'
-        #app.run(host=os.environ['ROS_IP'], port=5002)
+    #app.run(host=os.environ['ROS_IP'], port=5002)
 	#serve(app)
 	#serve(app, host=os.environ['ROS_IP'], port=5002)
 	#serve(app, host='0.0.0.0', port=5002)
@@ -74,4 +73,6 @@ if __name__ == '__main__':
         pkey_file = os.path.join(dirpath, '../config/ssl_keys/private-key.pem')
         app.run(host=os.environ['ROS_IP'], port=5000,
                 ssl_context=(cert_file, pkey_file))
+    rospy.Subscriber("/test_flag",Bool,test_cb)
+    rospy.spin()
     
